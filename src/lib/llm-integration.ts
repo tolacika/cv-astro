1e99d93c40c179d307b597d4e6154b80b472bce2
tolacika <1866861+tolacika@users.noreply.github.com>
2026-04-12 18:27:23 +0200

    fix llm context

--- src/lib/llm-integration.ts
+++ src/lib/llm-integration.ts
@@ -9,24 +9,34 @@ export function llmContextIntegration(): AstroIntegration {
 
     hooks: {
       "astro:build:done": async ({ dir }) => {
-        const htmlPath = path.join(dir.pathname, "llm.html");
-        const prompt = await fs.promises.readFile(htmlPath, "utf-8");
+        const config = {
+          "llm.html": [
+            "llm.txt",
+            "context.md",
+            "content.md",
+            "context.txt",
+            "content.txt",
+            "ai-context.txt"
+          ],
+          "llm-app.html": [
+            "llm-app.txt"
+          ],
+          "llm-res.html": [
+            "llm-res.txt"
+          ]
+        }
 
-        const files = [
-          "llm.txt",
-          "context.md",
-          "content.md",
-          "context.txt",
-          "content.txt",
-          "ai-context.txt",
-        ];
-        
-        for (const file of files) {
-          await fs.promises.writeFile(
-            path.join(dir.pathname, file),
-            prompt,
-            "utf-8"
-          );
+        for (const [source, dest] of Object.entries(config)) {
+          const htmlPath = path.join(dir.pathname, source);
+          const prompt = await fs.promises.readFile(htmlPath, "utf-8");
+
+          for (const file of dest) {
+            await fs.promises.writeFile(
+              path.join(dir.pathname, file),
+              prompt,
+              "utf-8"
+            );
+          }
         }
       },
     },
