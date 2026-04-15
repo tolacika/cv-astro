09fbee5524e09f767ff1669171b4e341b0280f7d
tolacika <1866861+tolacika@users.noreply.github.com>
2026-04-15 08:57:35 +0200

    fix

--- src/content/config.ts
+++
@@ -1,8 +0,0 @@
-import { glob } from "astro/loaders";
-import { defineCollection } from "astro:content";
-
-const post = defineCollection({
-  loader: glob({base:"./src/content/post", pattern:"**/*.{md,mdx}"})
-});
-
-export const collections = {};
